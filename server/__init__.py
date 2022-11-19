ff065f7aed0b10f787717356db5766339a7d419e
ducanhptitb19cn028 <99241227+ducanhptitb19cn028@users.noreply.github.com>
2022-11-19 18:37:15 +0700

    merge process

--- server/__init__.py
+++ server/__init__.py
@@ -32,29 +32,29 @@ while True:
 # cv2.imshow("imgo", imgo)
 
 # Removing the background
-    height, width = imgo.shape[:2]
-
-# Create a mask holder
-    mask = np.zeros(imgo.shape[:2], np.uint8)
-
-# Grab Cut the object
-    bgdModel = np.zeros((1, 65), np.float64)
-    fgdModel = np.zeros((1, 65), np.float64)
-
-# Hard Coding the Rect… The object must lie within this rect.
-    rect = (10, 10, width - 30, height - 30)
-    cv2.grabCut(imgo, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
-    mask = np.where((mask == 2) | (mask == 0), 0, 1).astype("uint8")
-    img1 = imgo * mask[:, :, np.newaxis]
-
-# Get the background
-    background = cv2.absdiff(imgo, img1)
-
-# Change all pixels in the background that are not black to white
-    background[np.where((background > [0, 0, 0]).all(axis=2))] = [255, 255, 255]
+#     height, width = imgo.shape[:2]
+#
+# # Create a mask holder
+#     mask = np.zeros(imgo.shape[:2], np.uint8)
+#
+# # Grab Cut the object
+#     bgdModel = np.zeros((1, 65), np.float64)
+#     fgdModel = np.zeros((1, 65), np.float64)
+#
+# # Hard Coding the Rect… The object must lie within this rect.
+#     rect = (10, 10, width - 30, height - 30)
+#     cv2.grabCut(imgo, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+#     mask = np.where((mask == 2) | (mask == 0), 0, 1).astype("uint8")
+#     img1 = imgo * mask[:, :, np.newaxis]
+#
+# # Get the background
+#     background = cv2.absdiff(imgo, img1)
+#
+# # Change all pixels in the background that are not black to white
+#     background[np.where((background > [0, 0, 0]).all(axis=2))] = [255, 255, 255]
 
 # Add the background and the image
-    final = background + img1
+    final = imgo
 
 # To be done – Smoothening the edges….
 
@@ -136,10 +136,10 @@ while True:
         # cv.destroyAllWindows()
 
     if flag == 0:
-        stri = "No one matches this finger print!!"
+        stri = "No one matches this fingerprint!!"
         # print(stri)
     print(stri)
-    # stri = "No one matches this finger print!!"
+    # stri = "No one matches this fingerprint!!"
     # print(stri)
 
     client_socket = socket.socket()  # instantiate
